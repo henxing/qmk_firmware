4af89198f617176d35d4b450105a82826e6a637b
Hugh Enxing <hugh@berkshiregrey.com>
2019-05-31 16:06:36 -0400

    Adds hotkeys

--- keyboards/planck/keymaps/henxing/keymap.c
+++ keyboards/planck/keymaps/henxing/keymap.c
@@ -55,9 +55,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_NO,   KC_NO,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_NO,   KC_NO,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_NO,   KC_NO,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, TG(_ADJUST), KC_NO,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  TG(_LOWER), KC_NO,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, TG(_QWERTY), KC_NO,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
   KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,  KC_LCTL,  KC_LALT, KC_SPC, RAISE,  KC_MINS, KC_QUOT, KC_ENT
 ),
 
@@ -68,9 +68,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * lower  insert super shift bksp ctrl || alt space   fn    .     0    =
  */
 [_RAISE] = LAYOUT_planck_grid(
-  KC_EXLM,   KC_AT,   KC_UP,   KC_LCBR, KC_RCBR, KC_NO,   KC_NO,   KC_PGUP, KC_7,    KC_8,   KC_9, KC_ASTR,
-  KC_HASH,   KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,  KC_NO,   KC_NO,   KC_PGDN, KC_4,    KC_5,   KC_6, KC_PLUS,
-  KC_LBRC,   KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR, KC_NO,   KC_NO,   KC_GRV,  KC_1,    KC_2,   KC_3, KC_BSLS,
+  KC_EXLM,   KC_AT,   KC_UP,   KC_LCBR, KC_RCBR, KC_TRNS, KC_TRNS, KC_PGUP, KC_7,    KC_8,   KC_9, KC_ASTR,
+  KC_HASH,   KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,  KC_TRNS, KC_TRNS, KC_PGDN, KC_4,    KC_5,   KC_6, KC_PLUS,
+  KC_LBRC,   KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR, KC_TRNS, KC_TRNS, KC_GRV,  KC_1,    KC_2,   KC_3, KC_BSLS,
   TG(_LOWER), KC_INS, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL
 ),
 
@@ -81,9 +81,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *       voldn  super shift bksp ctrl || alt space   L0  prtsc scroll pause
  */
 [_LOWER] = LAYOUT_planck_grid(
-  KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_NO,   KC_NO,   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-  KC_DELT, KC_LEFT, KC_DOWN, KC_RGHT, KC_DOWN, KC_NO,   KC_NO,   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11,
-  KC_TRNS, KC_VOLU, KC_TRNS, KC_TRNS, RESET,   KC_NO,   KC_NO,   KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F12,
+  KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_TRNS, KC_TRNS, KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+  KC_DELT, KC_LEFT, KC_DOWN, KC_RGHT, KC_DOWN, KC_TRNS, KC_TRNS, KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11,
+  KC_TRNS, KC_VOLU, KC_TRNS, KC_TRNS, RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F12,
   KC_TRNS, KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC, DF(_QWERTY), KC_PSCR, KC_SLCK, KC_PAUS
 ),
 
