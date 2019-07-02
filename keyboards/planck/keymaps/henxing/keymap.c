fe815a629069f927bb5899493ec1f25c5d9d9249
Hugh Enxing <hugh@berkshiregrey.com>
2019-07-02 15:45:29 -0400

    Updatesssss

--- keyboards/planck/keymaps/henxing/keymap.c
+++ keyboards/planck/keymaps/henxing/keymap.c
@@ -21,25 +21,15 @@ extern keymap_config_t keymap_config;
 
 enum planck_layers {
   _QWERTY,
-  _COLEMAK,
-  _DVORAK,
   _LOWER,
-  _RAISE,
-  _PLOVER,
-  _ADJUST
+  _RAISE
 };
 
 enum planck_keycodes {
   QWERTY = SAFE_RANGE,
-  COLEMAK,
-  DVORAK,
-  PLOVER,
-  BACKLIT,
-  EXT_PLV
+  LOWER,
+  RAISE
 };
-
-#define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -54,79 +44,84 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |  Esc |  Tab | GUI  | Shift| Bksp | Ctrl |  Alt |Space |Raise |  -   |   '  |Enter |
  * `-----------------------------------------------------------------------------------'
  */
-[_QWERTY] = LAYOUT_planck_grid(
-  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, TG(_ADJUST), KC_NO,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  TG(_LOWER), KC_NO,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, TG(_QWERTY), KC_NO,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-  KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,  KC_LCTL,  KC_LALT, KC_SPC, RAISE,  KC_MINS, KC_QUOT, KC_ENT
+[_QWERTY] = KEYMAP(
+  KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,    KC_NO,   KC_NO,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,
+  KC_A,   KC_S,   KC_D,    KC_F,    KC_G,    KC_NO,   KC_NO,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN,
+  KC_Z,   KC_X,   KC_C,    KC_V,    KC_B,    KC_NO,   KC_NO,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH,
+  KC_ESC, KC_TAB, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC, LOWER, KC_MINS, KC_QUOT, KC_ENT
 ),
 
-/*
- *  !       @     up     {    }        ||     pgup    7     8     9    *
- *  #     left   down  right  $        ||     pgdn    4     5     6    +
- *  [       ]      (     )    &        ||       `     1     2     3    \
- * lower  insert super shift bksp ctrl || alt space   fn    .     0    =
- */
-[_RAISE] = LAYOUT_planck_grid(
-  KC_EXLM,   KC_AT,   KC_UP,   KC_LCBR, KC_RCBR, KC_TRNS, KC_TRNS, KC_PGUP, KC_7,    KC_8,   KC_9, KC_ASTR,
-  KC_HASH,   KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,  KC_TRNS, KC_TRNS, KC_PGDN, KC_4,    KC_5,   KC_6, KC_PLUS,
-  KC_LBRC,   KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR, KC_TRNS, KC_TRNS, KC_GRV,  KC_1,    KC_2,   KC_3, KC_BSLS,
-  TG(_LOWER), KC_INS, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL
-),
-
-/*
- * insert home   up  end   pgup       ||      up     F7    F8    F9   F10
- *  del   left  down right pgdn       ||     down    F4    F5    F6   F11
- *       volup             reset      ||             F1    F2    F3   F12
- *       voldn  super shift bksp ctrl || alt space   L0  prtsc scroll pause
- */
-[_LOWER] = LAYOUT_planck_grid(
-  KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_TRNS, KC_TRNS, KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-  KC_DELT, KC_LEFT, KC_DOWN, KC_RGHT, KC_DOWN, KC_TRNS, KC_TRNS, KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11,
-  KC_TRNS, KC_VOLU, KC_TRNS, KC_TRNS, RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F12,
-  KC_TRNS, KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC, DF(_QWERTY), KC_PSCR, KC_SLCK, KC_PAUS
-),
-
-/* Adjust (Lower + Raise)
+/* Lower
  * ,-----------------------------------------------------------------------------------.
- * |      | Reset|      |      |      |      |      |      |      |      |      |  Del |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|Plover|      |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|      |      |      |      |      |
+ * |   !  |   @  |  up  |   {  |   }  |      |      | pgup |   7  |   8  |   9  |   *  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
+ * |   #  | left | down | right|   $  |      |      | pgdn |   4  |   5  |   6  |   +  |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |   [  |   ]  |   (  |   )  |   &  |      |      |   `  |   1  |   2  |   3  |   \  |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | lower|  ins | super| shift| bksp | ctrl | alt  |space |  fn  |   .  |   0  |   =  |
  * `-----------------------------------------------------------------------------------'
  */
-[_ADJUST] = LAYOUT_planck_grid(
-    _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
-    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  QWERTY,   QWERTY,  QWERTY,  _______,
-    _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
+[_LOWER] = KEYMAP(
+  KC_EXLM, KC_AT,   KC_UP,   KC_LCBR, KC_RCBR, KC_TRNS, KC_TRNS, KC_PGUP, KC_7,    KC_8,   KC_9, KC_ASTR,
+  KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,  KC_TRNS, KC_TRNS, KC_PGDN, KC_4,    KC_5,   KC_6, KC_PLUS,
+  KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR, KC_TRNS, KC_TRNS, KC_GRV,  KC_1,    KC_2,   KC_3, KC_BSLS,
+  RAISE,   KC_INS,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL
+),
+
+/* Raise
+ * ,-----------------------------------------------------------------------------------.
+ * | ins  | home |  up  |  end | pgup |      |      |  up  |  F7  |  F8  |  F9  |  F10 |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |  del | left | down | right| pgdn |      |      | down |  F4  |  F5  |  F6  |  F11 |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |volup |      |      |reset |      |      |      |  F1  |  F2  |  F3  |  F12 |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |voldn | super| shift| bksp | ctrl | alt  |space |  L0  | prtsc|scroll| pause|
+ * `-----------------------------------------------------------------------------------'
+ */
+[_RAISE] = KEYMAP(
+  KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_TRNS, KC_TRNS, KC_UP,   KC_F7,  KC_F8,   KC_F9,   KC_F10,
+  KC_DELT, KC_LEFT, KC_DOWN, KC_RGHT, KC_DOWN, KC_TRNS, KC_TRNS, KC_DOWN, KC_F4,  KC_F5,   KC_F6,   KC_F11,
+  KC_TRNS, KC_VOLU, KC_TRNS, KC_TRNS, RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_F1,  KC_F2,   KC_F3,   KC_F12,
+  KC_TRNS, KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  QWERTY, KC_PSCR, KC_SLCK, KC_PAUS
 )
 };
 
-const uint16_t PROGMEM fn_actions[] = {
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
-};
+  // The value to return
+  bool return_value = false;
 
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-  switch(id) {
-    case 1:
+  switch (keycode) {
+    case QWERTY:
       if (record->event.pressed) {
-        layer_on(1);
-      } else {
-        layer_off(1);
+        set_single_persistent_default_layer(_QWERTY);
       }
       break;
-    case 2:
+
+    case LOWER:
+      // Toggle LOWER layer on when key pressed and off when released
       if (record->event.pressed) {
-        layer_on(2);
+        layer_on(_LOWER);
       } else {
-        layer_off(2);
+        layer_off(_LOWER);
       }
+      break;
+
+    case RAISE:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_RAISE);
+      }
+      break;
+
+    default:
+
+      // If the keycode is not handled by any of the other cases, the
+      // function should return true
+      return_value = true;
       break;
   }
-  return MACRO_NONE;
-};
+
+  return return_value;
+}
